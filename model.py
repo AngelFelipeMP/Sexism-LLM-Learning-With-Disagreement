b830fbc284c06490c395f5b09801f053e6f5e342
AngelFelipeMP <angel.magnossao@gmail.com>
2023-05-03 06:08:51 +0200

    adapet soft max for tasks 1 and 2

--- model.py
+++ model.py
@@ -10,15 +10,22 @@ class TransforomerModel(nn.Module):
         self.transformer = AutoModel.from_pretrained(transformer)
         self.dropout = nn.Dropout(drop_out)
         self.classifier = nn.Linear(self.embedding_size * 2, self.number_of_classes)
+        self.softmax = nn.Softmax(dim=1)
         
     def forward(self, iputs):
         transformer_output  = self.transformer(**iputs)
-        #TODO: check if the best way to generate LLM final vector is to average the last hidden state
-        #TODO: how to have different learning rate for the LLM and for the classifier 
-        #TODO: I guess I will have to brack the model into two pieces
         mean_pool = torch.mean(transformer_output['last_hidden_state'], 1)
         max_pool, _ = torch.max(transformer_output['last_hidden_state'], 1)
         cat = torch.cat((mean_pool,max_pool), 1)
         drop = self.dropout(cat)
+        output = self.classifier(drop)
+        
+        # it means task 3 -> Multi-label classification
+        if iputs['targets'].shape[1] > 3:
+            output = self.softmax(output)
 
-        return self.classifier(drop)
+        return output
+    
+    #COMMENT: check if the best way to generate LLM final vector is to average the last hidden state
+    #COMMENT: how to have different learning rate for the LLM and for the classifier 
+    #COMMENT: I guess I will have to brack the model into two pieces
