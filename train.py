1308d133d64bf3d126f4e9573b301f4a7a0a557d
AngelFelipeMP <angel.magnossao@gmail.com>
2023-05-04 09:06:27 +0200

    train.py adptation draft 2

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import random
 import config
 from tqdm import tqdm
 import argparse
+from utils import transformation, save_preds, eval_preds
 
 from model import TransforomerModel
 import warnings
@@ -54,7 +55,7 @@ def train(df_train, df_val, task, epochs, best_epoch, transformer, max_len, batc
     model.to(device)
     
     #NOTE: I must check appropriate no_decay for LLaMA
-    #TODO: differte learning rates for LLaMA and classifier
+    #NOTE: differte learning rates for LLaMA and classifier
     param_optimizer = list(model.named_parameters())
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
     optimizer_parameters = [
@@ -69,17 +70,14 @@ def train(df_train, df_val, task, epochs, best_epoch, transformer, max_len, batc
     )
     
     for epoch in range(1, best_epoch+1):
-        pred_train, targ_train, loss_train = engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
-        #TODO: replace f1 score and accuracy for (ICM-soft)
-        # f1_train = metrics.f1_score(targ_train, pred_train, average='macro')
-        # acc_train = metrics.accuracy_score(targ_train, pred_train)
         
-        pred_val, targ_val, loss_val = engine.eval_fn(val_data_loader, model, device)
-        #TODO: replace f1 score and accuracy for (ICM-soft)
-        # f1_train = metrics.f1_score(targ_train, pred_train, average='macro')
-        # acc_train = metrics.accuracy_score(targ_train, pred_train)
+        pred_train, _ , loss_train = engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
+        save_preds(pred_train, df_train, task, data, 'training', epoch, transformer)
+        icm_soft_train = eval_preds(task, data, 'training', epoch, transformer)
         
-        
+        pred_val, _ , loss_val = engine.eval_fn(val_data_loader, model, device)
+        save_preds(pred_val, df_val, task, data, 'dev', epoch, transformer)
+        icm_soft_val = eval_preds(task, data, 'dev', epoch, transformer)
         
         df_new_results = pd.DataFrame({'task':task,
                             'epoch':epoch,
@@ -88,26 +86,20 @@ def train(df_train, df_val, task, epochs, best_epoch, transformer, max_len, batc
                             'batch_size':batch_size,
                             'lr':lr,
                             'dropout':drop_out,
-                            #TODO: replace f1 score and accuracy for (ICM-soft)
-                            # 'accuracy_train':acc_train,
-                            # 'f1-macro_train':f1_train,
+                            'icm_soft_train': icm_soft_train,
                             'loss_train':loss_train,
-                            #TODO: replace f1 score and accuracy for (ICM-soft)
-                            # 'accuracy_val':acc_val,
-                            # 'f1-macro_val':f1_val,
+                            'icm_soft_val':icm_soft_val,
                             'loss_val':loss_val
                         }, index=[0]
         )
         
-        #TODO: I must check it: why pd.concat?
         df_results = pd.concat([df_results, df_new_results], ignore_index=True)
         
-        # tqdm.write("Epoch {}/{} f1-macro_training = {:.3f}  accuracy_training = {:.3f}  loss_training = {:.3f} f1-macro_val = {:.3f}  accuracy_val = {:.3f}  loss_val = {:.3f}".format(epoch, config.EPOCHS, f1_train, acc_train, loss_train, f1_val, acc_val, loss_val))
-        tqdm.write("Epoch {}/{} ICM-soft_training = {:.3f} loss_training = {:.3f} ICM-soft_val = {:.3f}  loss_val = {:.3f}".format(epoch, config.EPOCHS, icm-soft_train, loss_train, icm-soft_val, loss_val))
+        tqdm.write("Epoch {}/{} ICM-soft_training = {:.3f} loss_training = {:.3f} ICM-soft_val = {:.3f}  loss_val = {:.3f}".format(epoch, config.EPOCHS, icm_soft_train, loss_train, icm_soft_val, loss_val))
 
-    #TODO: save best model
     # add "language" as input
-    torch.save(model.state_dict(), f'{config.LOGS_PATH}/{data}_task[{task}]_transformer[{transformer.split("/")[-1]}]_epoch[{epoch}]_maxlen[{max_len}]_batchsize[{batch_size}]_dropout[{drop_out}]_lr[{lr}]_language[{language}].model')
+    path_model = config.LOGS_PATH + '/model' + '_' + task + '_' + data + '_' + transformer.split("/")[-1] + '.pt'
+    torch.save(model.state_dict(), path_model)
 
     return df_results
 
@@ -117,16 +109,13 @@ if __name__ == "__main__":
     parser.add_argument('--data', type=str, help='Data/Datasets to train the models')
     args = parser.parse_args()
     
-    #TODO: check if I need "domain"
-    #TODO: if I need I must fix them
     if args.data == 'training':
-        datasets = [config.DATASET_TRAIN]
+        datasets = config.DATASET_TRAIN
         domain = config.DOMAIN_TRAIN
     
     elif args.data == 'training-dev':
-        #TODO I have already joing it, chnage things here
-        datasets = [config.DATASET_TRAIN, config.DATASET_DEV]
-        domain = config.DOMAIN_TRAIN_ALL_DATA
+        datasets = config.DATASET_TRAIN_DEV
+        domain = config.DOMAIN_TRAIN_DEV
     
     elif not args.data:
         print('Specifying --data is required')
@@ -142,14 +131,11 @@ if __name__ == "__main__":
     torch.manual_seed(config.SEED)
     torch.cuda.manual_seed_all(config.SEED)
     
-    dataset_list = []
-    for data in datasets:
-        #TODO: I must check index_col=None
-        df = pd.read_csv(config.DATA_PATH + '/' + data, index_col=None)
-        dataset_list.append(df)
+    df = pd.read_csv(config.DATA_PATH + '/' + datasets, index_col=None)
     
     #TODO: I have already joing it, chnage things here
-    dfx = pd.concat(dataset_list, axis=0, ignore_index=True).iloc[:config.N_ROWS]
+    #TODO: dfx should be df
+    # dfx = pd.concat(dataset_list, axis=0, ignore_index=True).iloc[:config.N_ROWS]
     print('Dataset shape: ', dfx.shape)
     
     df_results = pd.DataFrame(columns=['task',
@@ -159,13 +145,9 @@ if __name__ == "__main__":
                                     'batch_size',
                                     'lr',
                                     'dropout',
-                                    #TODO: replace f1 score and accuracy for (ICM-soft)
-                                    # 'accuracy_train',
-                                    # 'f1-macro_train',
+                                    'icm_soft_train',
                                     'loss_train',
-                                    #TODO: replace f1 score and accuracy for (ICM-soft)
-                                    # 'accuracy_val',
-                                    # 'f1-macro_val',
+                                    'icm_soft_val',
                                     'loss_val'
         ]
     )
@@ -187,7 +169,12 @@ if __name__ == "__main__":
                             config.LR,
                             config.DROPOUT,
                             df_results,
-                            domain
+                            args.data
+                            #domain
         )
             
         df_results.to_csv(config.LOGS_PATH + '/' + domain + '.csv', index=False)
+        
+        
+        
+        #TODO: REVIEL ALL THIS SCRIPT AGAIN
