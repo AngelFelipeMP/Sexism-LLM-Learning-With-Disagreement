9f69fdfed4ca84160b98b5d1491ed2f47db38782
Damiano Spina <damiano.spina@rmit.edu.au>
2023-10-06 06:27:25 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,27 @@
 import os
-#Hiper-parameters
-EPOCHS = 10 #10
-MAX_LEN = 100 # 100 
+#Hyperparameters
+EPOCHS = 10 
+MAX_LEN = 100
 DROPOUT = 0.3
 LR = 3e-5 #5e-6, 1e-5, 3e-5, 5e-5
 BATCH_SIZE = 20 #24
 TRANSFORMERS = ['bert-base-multilingual-uncased','xlm-roberta-base']
-# TRANSFORMERS = ['bert-base-multilingual-uncased','xlm-roberta-base','infoxlm-base']
 
 N_ROWS=None #16
 SEED = 17
 CODE_PATH = os.getcwd()
 REPO_PATH = '/'.join(CODE_PATH.split('/')[0:-1])
-DATA_PATH = REPO_PATH + '/' + 'data' 
+DATA_PATH = REPO_PATH + '/' + 'data'
 PACKAGE_PATH = REPO_PATH + '/' + 'package'
 DATA_DEV_PATH = PACKAGE_PATH + '/' + 'dev/EXIST2023_dev' + '.json'
 DATA_TRAIN_PATH = PACKAGE_PATH + '/' + 'training/EXIST2023_training' + '.json'
 DATA_TEST_PATH = PACKAGE_PATH + '/' + 'test/EXIST2023_test_clean' + '.json'
 LABEL_GOLD_PATH = PACKAGE_PATH + '/' + 'evaluation/golds'
 
-DATA = 'EXIST2023' 
-DATA_URL = 'https://drive.google.com/file/d/11WxMMTyZibk6hWNSp2UeVFTllFcgfs52/view?usp=share_link'
+DATA = 'EXIST2023'
+DATA_URL = 'URL_SHARED_BY_ORGANIZERS_TO_DOWNLOAD_EXIST_2023_DATA'
 
-LABELS = ['task1', 'task2','task3'] #['task1', 'task2','task3']
+LABELS = ['task1', 'task2','task3']
 COLUMN_TEXT = 'tweet'
 COLUMN_LABELS = 'soft_label_'
 DATASET_INDEX = 'id_EXIST'
